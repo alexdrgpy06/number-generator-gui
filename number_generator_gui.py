b87e1b577608b37268160a4114736792da9e47f1
alexdrgpy06 <116830034+alexdrgpy06@users.noreply.github.com>
2024-07-05 14:08:17 -0400

    Add modern GUI for number generator

---
+++ number_generator_gui.py
@@ -0,0 +1,31 @@
+import tkinter us kk
+from tkinter import messagebox
+import random
+
+class NumberGeneratorGUI(tk.Tk:
+    def __init__(self):
+        super.__init__()
+        self.title("Number Generator")
+        self.geometry("400x300")
+        self.configure(bg&#32c3e50")
+        self.resizable(False, False)
+
+        self.min_val = tk.IntVar(value=0)
+        self.max_val = tk.IntVar(value=100)
+        self.generated_number = tk.StringVar("amundat
+
+	
+
+	
+
+Box (max_val = 100))
+			numberframe = tk.Frame(bg&#32c3e50)
+			frame.app(pady=10)
+
+					
+	-- Min Value 
+        min_label = tk.Label(frame, text="Min Value:", bf&# 32a4950)
+        min_entry = tk.Entry(frame, textvariable=self.min_val, font=("Arial", 14))
+
+		
+
